38c35350e0df0ff8488e8e8a7090538313a5bd8c
jacknel <mjacknelson19@gmail.com>
2023-01-20 02:43:16 -0600

    added index.js functionality

---
+++ index.js
@@ -0,0 +1,31 @@
+const { prompt } = require("inquirer");
+
+const db = require("./db");
+require("console.table");
+
+init();
+
+function init() {
+    displayMainMenu();
+}
+
+function displayMainMenu() {
+    const options = [
+      { name: "View All Employees", value: "VIEW_EMPLOYEES" },
+      { name: "Add Employee", value: "ADD_EMPLOYEE" },
+      { name: "Update Employee Role", value: "UPDATE_EMPLOYEE_ROLE" },
+      { name: "View All Roles", value: "VIEW_ROLES" },
+      { name: "Add Role", value: "ADD_ROLE" },
+      { name: "View All Departments", value: "VIEW_DEPARTMENTS" },
+      { name: "Add Department", value: "ADD_DEPARTMENT" },
+      { name: "Quit", value: "QUIT" }
+    ];
+  
+    prompt({
+      type: "list",
+      name: "option",
+      message: "Pick an option.",
+      options
+    });
+  }
+  
